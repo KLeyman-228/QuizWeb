66feb916a38b9a127b572ec8556e98c1b194ffbc
kleyman-228 <llejmon@gmail.com>
2026-04-10 23:34:48 +0300

    namefix

--- GameQuiz/asgi.py
+++ GameQuiz/asgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamequiz.settings")
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
